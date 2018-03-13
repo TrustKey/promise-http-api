8316f460b7ed90115d0562ea459b3961e396e505
Igor Nikolaychuk <jmpeax@icloud.com>
2018-03-13 23:34:41 +0200

    updated for new promise api

--- README.md
+++ README.md
@@ -9,8 +9,8 @@ Also provides `GET /promise/resolve` and `GET /promise/reject` methods with `b64
 ### Request examples with Curl:
 
 ```bash
-curl -d '{"promise_alg":"random_int_rs0","min":0,"max":10,"b64_buffers":true,"seed":"T3XYDzsGx3mHWaX35DjKZ5jIc9/abiwBvBHVKYnlqBLSSM0WhsOWrDNQrpfUGZuVAYUre57Um98Zz1bUyMHq+Q=="}' -H"Content-Type: application/json" -X POST http://127.0.0.1:3000/promise/resolve
-curl http://127.0.0.1:3000/promise/resolve\?b64_body\=ew0KCSJwcm9taXNlX2FsZyI6ICJyYW5kb21faW50X3JzMCIsDQoJIm1pbiI6IDAsDQoJIm1heCI6IDEwLA0KCSJiNjRfYnVmZmVycyI6IHRydWUsDQoJInNlZWQiOiAiVDNYWUR6c0d4M21IV2FYMzVEaktaNWpJYzkvYWJpd0J2QkhWS1lubHFCTFNTTTBXaHNPV3JETlFycGZVR1p1VkFZVXJlNTdVbTk4WnoxYlV5TUhxK1E9PSINCn0=
+curl -d '{"_f":"random_int_rs0","min":0,"max":10,"b64_buffers":true,"seed":"T3XYDzsGx3mHWaX35DjKZ5jIc9/abiwBvBHVKYnlqBLSSM0WhsOWrDNQrpfUGZuVAYUre57Um98Zz1bUyMHq+Q=="}' -H"Content-Type: application/json" -X POST http://127.0.0.1:3000/promise/resolve
+curl http://127.0.0.1:3000/promise/resolve\?b64_body\=eyJfZiI6InJhbmRvbV9pbnRfcnMwIiwibWluIjowLCJtYXgiOjEwLCJiNjRfYnVmZmVycyI6dHJ1ZSwic2VlZCI6IlQzWFlEenNHeDNtSFdhWDM1RGpLWjVqSWM5L2FiaXdCdkJIVktZbmxxQkxTU00wV2hzT1dyRE5RcnBmVUdadVZBWVVyZTU3VW05OFp6MWJVeU1IcStRPT0ifQ==
 ```
 
 [0]: https://github.com/TrustKey/supervisor
--- module.js
+++ module.js
@@ -5,28 +5,18 @@ module.exports = function setup(options, imports, register) {
     let app = httpServer.app;
     let promise = imports.promise;
 
-    const PromiseMethod = {
-        create: 0,
-        resolve: 1
-    };
-
-
-    const promiseHandler = (method) => (req, resp) => {
+    const promiseHandler = (req, resp) => {
         let body = req.body;
         body.b64_buffers = true;
 
-        if(method === PromiseMethod.create)
-            promise.createPromise(body, res => resp.send(res));
-        else
-            promise.resolvePromise(body, res => resp.send(res));
+        promise.resolve(body, res => resp.send(res));
     };
 
 
-    app.post('/promise/create', promiseHandler(PromiseMethod.create));
-    app.post('/promise/resolve', promiseHandler(PromiseMethod.resolve));
+    app.post('/promise/resolve', promiseHandler);
 
 
-    const b64ProxyHandler = (method) => (req, resp) => {
+    const b64ProxyHandler = (req, resp) => {
         if(!req.query.b64_body)
             return resp.status(500).send({success: false, error: "No b64_body query param"});
 
@@ -34,15 +24,14 @@ module.exports = function setup(options, imports, register) {
             const jsonString = new Buffer(req.query.b64_body, 'base64').toString('ascii');
             const body = JSON.parse(jsonString);
 
-            return promiseHandler(method)({body: body}, resp);
+            return promiseHandler({body: body}, resp);
         }
         catch (e) {
             resp.status(500).send({success: false, error: JSON.stringify(e.message)});
         }
     };
 
-    app.get('/promise/create', b64ProxyHandler(PromiseMethod.create));
-    app.get('/promise/resolve', b64ProxyHandler(PromiseMethod.resolve));
+    app.get('/promise/resolve', b64ProxyHandler);
 
     register(null, { });
 };
